22cd8597d9408f5e3eef78d0c3fbeb2768ead259
PadmaDhungel <padma.qcoder@gmail.com>
2024-12-15 11:42:37 +1100

    Initialize Next.js project with shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+const inter = Inter({subsets: ['latin']})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn("relative h-full font-sans antialiased",inter.className )}
       >
-        {children}
+        <main className= "relative flex flex-col min-h-screen">
+        <div className="flex-grow flex-1">{children}</div>
+        </main>
       </body>
     </html>
   );
